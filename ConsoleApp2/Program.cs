6849ad5c499508c6fed4e5e6f2d255de0515161f
edivaldo.junior <er.moura88@yahoo.com.br>
2020-10-05 16:06:49 -0300

    Adicionar arquivos de projeto.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            String nomePessoa;
+
+            Console.WriteLine("Digite o seu Nome: ");
+            nomePessoa = Console.ReadLine();
+            Console.ReadKey();
+
+            /*
+            Console.WriteLine("Edivaldo Junior"); //WriteLine - o cursor fica abaixo do comando (quebra de linha)
+            Console.ReadKey(); //ReadKey - função que vai parar o cmd até o usuário apertar alguma tecla
+            */
         }
     }
 }
