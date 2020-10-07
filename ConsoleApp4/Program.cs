fbd660ee1b8b7f6003d3927d6c579767fd49cb62
edijunior88 <er.moura88@yahoo.com.br>
2020-10-07 20:48:01 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String nomePessoa;
+            String sobrenomePessoa;
+
+            Console.WriteLine("Digite seu nome: ");
+            nomePessoa = Console.ReadLine();
+
+            Console.WriteLine("Digite seu sobrenome: ");
+            sobrenomePessoa = Console.ReadLine();
+
+            Console.WriteLine("O nome escrito foi " + nomePessoa + " " + sobrenomePessoa);
+            Console.ReadKey();
+        }
+    }
+}
