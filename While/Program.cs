4c33144cf9af1a406a77222309617b1ecc75ab3c
edijunior88 <er.moura88@yahoo.com.br>
2020-10-08 15:53:18 -0300

    Adicionar arquivos de projeto.

---
+++ While/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //CÓDIGO NÃO FINALIZADO
+
+            double numero;
+            double total;
+            String operacao;
+
+            total = 0;
+            operacao = "+";
+
+            while (operacao != "=")
+            {
+                Console.Write("Entre com um valor ");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+                switch (operacao)
+                {
+                    case "+": total = total + numero;
+                        break;
+
+                    case "-": total = total - numero;
+                        break;
+
+                    case "*": total = total * numero;
+                        break;
+
+                    case "/": total = total / numero;
+                        break;
+                }
+
+
+            }
+
+            Console.Write("Digite o primeiro número: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a operação: ");
+            operacao = Console.ReadLine();
+
+            Console.Write("Digite o segundo número: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" ");
+
+            Console.WriteLine("O resultado é: ");
+            Console.ReadKey();
+        }
+    }
+}
