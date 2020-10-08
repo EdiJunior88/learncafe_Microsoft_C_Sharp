32089f64674e342737a5da0b293ca3eedbc17d94
edijunior88 <er.moura88@yahoo.com.br>
2020-10-08 11:06:35 -0300

    Adicionar arquivos de projeto.

---
+++ Switch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            double soma;
+            String operacao;
+
+            Console.Write("Digite o primeiro número: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a operação: ");
+            operacao = Console.ReadLine();
+
+            Console.Write("Digite o segundo número: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            soma = 0;
+
+            switch (operacao)
+            {
+                case "+": soma = numero1 + numero2;
+                    break;
+
+                case "-": soma = numero1 - numero2;
+                    break;
+
+                case "*": soma = numero1 * numero2;
+                    break;
+
+                case "/": soma = numero1 / numero2;
+                    break;
+            }
+                
+            Console.WriteLine(" ");
+
+            Console.Write("O resultado é: " + soma);
+            Console.ReadKey();
+        }
+    }
+}
