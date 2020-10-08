a53d954b81b7082bdf3feece43a2889d38794b79
edijunior88 <er.moura88@yahoo.com.br>
2020-10-08 16:11:25 -0300

    Adicionar arquivos de projeto.

---
+++ Array/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Finalização do código da aula 09 e explicação sobre ARRAYS
+
+            double numero;
+            double total;
+            string operacao;
+
+            total = 0;
+            operacao = "+";
+
+            while(operacao != "=")
+            {
+                Console.Write("Entee com um valor: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+                switch (operacao)
+                {
+                    case "+": total = total + numero;
+                        break;
+
+                    case "-": total = total - numero;
+                        break;
+
+                    case "*": total = total * numero;
+                        break;
+
+                    case "/": total = total / numero;
+                        break;
+                }
+
+                Console.Write("Digite a operação: ");
+                operacao = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nO resultado é: " + total);
+            Console.ReadKey();
+        }
+    }
+}
