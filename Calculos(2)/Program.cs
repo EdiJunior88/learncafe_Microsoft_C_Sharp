6b3916906be7aa112caf72e6c61d616a62d84b56
edijunior88 <er.moura88@yahoo.com.br>
2020-10-07 22:06:48 -0300

    Adicionar arquivos de projeto.

---
+++ Calculos(2)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1;
+            double numero2;
+            double soma;
+            String operacao;
+
+            Console.Write("Digite o primeiro número: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write(" ");
+
+            Console.Write("Digite o segundo número: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+
+            soma = numero1 + numero2;
+
+            Console.Write(" ");
+
+            Console.Write("O resultado é: " + soma);
+            Console.ReadKey();
+        }
+    }
+}
