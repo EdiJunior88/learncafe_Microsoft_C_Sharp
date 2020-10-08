2b545cfa9ee3c137712f33c6aab8255c99748e87
edijunior88 <er.moura88@yahoo.com.br>
2020-10-07 21:11:03 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double numero1;
+            double numero2;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            numero1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            numero2 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
