c5dfa623640ccb485f4eb86c28b09d6127328122
edijunior88 <er.moura88@yahoo.com.br>
2020-10-13 15:45:09 -0300

    Adicionar arquivos de projeto.

---
+++ For/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome;
+            nome = new string[4];
+
+            nome[0] = "Hugo";
+            nome[1] = "Vasconcelos";
+            nome[2] = "De";
+            nome[3] = "Freitas";
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(nome[i]);
+            }
+        }
+    }
+}
